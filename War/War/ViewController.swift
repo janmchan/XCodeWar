cedfa1e578447c2c2145d7070f97d3c1c1a93f63
janmchan <jan.m.chan@outlook.com>
2016-07-17 21:25:58 +1200

    added new logic and new image

--- War/War/ViewController.swift
+++ War/War/ViewController.swift
@@ -14,12 +14,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var secondCardImageView: UIImageView!
     @IBOutlet weak var playAroundButton: UIButton!
     @IBOutlet weak var backgroundImageView: UIImageView!
+    @IBOutlet weak var thirdCardImageView: UIImageView!
     
     @IBOutlet weak var enemyScoreLabel: UILabel!
     @IBOutlet weak var playerScoreLabel: UILabel!
     
     var playerScore:Int = 0
     var enemyScore:Int = 0
+    var hitMe:Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,7 +35,30 @@ class ViewController: UIViewController {
     }
 
     @IBAction func playRoundTapped(sender: UIButton) {
+        if !hitMe
+        {
+            //self.playAroundButton.setTitle("Play Round", forState: UIControlState.Normal)
+            let firstCard = arc4random_uniform(13) + 1
+            let secondCard = arc4random_uniform(13) + 1
+            let firstCardName = String(format: "card%i", firstCard)
+            let secondCardName = String(format: "card%i", secondCard)
+            self.firstCardImageView.image = UIImage(named: firstCardName)
+            self.secondCardImageView.image = UIImage(named: secondCardName)
+            hitMe = true
+            return
+        }
+        else
+        {
+            let thirdCard = arc4random_uniform(13) + 1
+            let thirdCardName = String(format: "card%i", thirdCard)
+            self.thirdCardImageView.image = UIImage(named: thirdCardName)
+            hitMe = true
+        }
         
+     }
+    
+    func oldLogic()
+    {
         //self.playAroundButton.setTitle("Play Round", forState: UIControlState.Normal)
         let firstCard = arc4random_uniform(13) + 1
         let secondCard = arc4random_uniform(13) + 1
@@ -53,7 +78,7 @@ class ViewController: UIViewController {
         }
         else
         {
-            enemyScore++
+            enemyScore += 1
             enemyScoreLabel.text = String(enemyScore)
         }
     }
